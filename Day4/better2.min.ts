2d5dd69f1b6018d41b3d12f9ddf77f0de1cbc6c1
TheCodedProf <samuel.shuert@gmail.com>
2022-12-04 01:31:04 -0500

    min min'd

--- Day4/better2.min.ts
+++ Day4/better2.min.ts
@@ -1,4 +1,4 @@
-import { readFileSync as rfs } from "fs";
+import{readFileSync as rfs}from"fs";
 let In=rfs('./input.txt', 'utf8').split('\r\n')
 const r=(s, e)=>Array.from({length:e-s+1},(_, i)=>s+i);
 console.log(((v=-1)=>{In.forEach(l=>l.split(',').map(x=>r(parseInt(x.split('-')[0]),parseInt(x.split('-')[1]))).some((_,i,[a,b])=>v+=(a.every(e=>b.includes(e))||b.every(e=>a.includes(e)))as any as number));return v})());
