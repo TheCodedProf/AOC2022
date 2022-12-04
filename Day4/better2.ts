f7fc62867a849d2ddb6290c6d0fcb6b6d89033bc
TheCodedProf <samuel.shuert@gmail.com>
2022-12-04 01:24:23 -0500

    Day4 'better' solutions fixed

--- Day4/better2.ts
+++ Day4/better2.ts
@@ -4,6 +4,6 @@ let input = readFileSync('./input.txt', 'utf8').split('\r\n')
 
 const range = (start: number, end: number) => Array.from({ length: end - start + 1 }, (_, i) => start + i);
 
-console.log(((valid: number = -1) => {input.forEach(line => line.split(',').map(x => range(parseInt(x.split('-')[0]), parseInt(x.split('-')[1]))).some((_, _i, [a,b]) => valid += (a.every(el => b.includes(el)) || b.every(el => a.includes(el))) as unknown as number)); return valid})(-1));
+console.log(((valid: number = -1) => {input.forEach(line => line.split(',').map(x => range(parseInt(x.split('-')[0]), parseInt(x.split('-')[1]))).some((_, _i, [a,b]) => valid += (a.every(el => b.includes(el)) || b.every(el => a.includes(el))) as unknown as number)); return valid})());
 
-console.log(((valid: number = -1) => {input.forEach(line => line.split(',').map(x => range(parseInt(x.split('-')[0]), parseInt(x.split('-')[1]))).some((_, _i, [a,b]) => valid += (a.some(el => b.includes(el))) as unknown as number)); return valid})(-1));
+console.log(((valid: number = -1) => {input.forEach(line => line.split(',').map(x => range(parseInt(x.split('-')[0]), parseInt(x.split('-')[1]))).some((_, _i, [a,b]) => valid += (a.some(el => b.includes(el))) as unknown as number)); return valid})());
